a583382900ea7d20806b3d5bf863e6d54c3370d6
Jonny Gerig Meyer <jonny@oddbird.net>
2011-06-27 15:55:40 -0600

    updated django-icanhaz 0.2.0 and django-html5accordion 0.1.1; installed django-messages-ui and django-ajax-loading-overlay

--- src/oddtools/settings/base.py
+++ src/oddtools/settings/base.py
@@ -146,8 +146,14 @@ LOGGING = {
     }
 }
 
+INSTALLED_APPS += ["html5accordion"]
+
+INSTALLED_APPS += ["messages_ui"]
+
+INSTALLED_APPS += ["ajax_loading_overlay"]
+
 INSTALLED_APPS += ["icanhaz"]
-ICANHAZ_DIR = join(BASE_PATH, "jstemplates")
+ICANHAZ_DIRS = [join(BASE_PATH, "jstemplates")]
 
 INSTALLED_APPS += ["compressor"]
 COMPRESS_CSS_FILTERS = ["compressor.filters.css_default.CssAbsoluteFilter",
--- static/js/local.js
+++ static/js/local.js
@@ -2,4 +2,5 @@ $(function() {
     $('#hcard-client-name .email').defuscate();
     $('input[placeholder], textarea[placeholder]').placeholder();
     $('.details:not(html)').html5accordion('.summary');
+    $('#messages').messages();
 });
